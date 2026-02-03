ab53c61c35a6196fb9bcadea1c6cd858eed542e7
Nikolay Vaklev <58629389+nikolaychv@users.noreply.github.com>
2026-02-03 15:09:42 +0200

    creating auth service

---
+++ src/main/java/bg/mechano/auth/MechanoAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package bg.mechano.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechanoAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechanoAuthServiceApplication.class, args);
+	}
+
+}
